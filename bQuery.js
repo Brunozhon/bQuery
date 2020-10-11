4705e8cd6e8c0452c31be01b934525b504a88b02
Bruno Zhong <69879040+Brunozhon@users.noreply.github.com>
2020-10-10 20:53:27 -0400

    A... ah... ahhh... ahhh... CHOO!, said the players
    A... ah... ahhh... ahhh... CHOO!, repeated the crowd

--- bQuery.js
+++ bQuery.js
@@ -11,8 +11,30 @@ var bQuery = {
     } else {
       document.attachEvent("on" + even, func)
     }
+  },
+  html: function(elem, text) {
+    elem.innerHTML = text;
+    if (elem === undefined || text === undefined) {
+      return elem.innerHTML;
+    }
+  },
+  text: function(elem, text) {
+    elem.innerText = text;
+    if (elem === undefined || text === undefined) {
+      return elem.innerText;
+    }
+  },
+  css: function(elem, obj) {
+    elem.style.setProperty(obj.name, obj.value)
+  },
+  $one: function(elem) {
+    return document.querySelector(elem)
   }
 }
 function noConfilct() {
-  return bQuery;
+  if (bQuery !== undefined) {
+    var copy = bQuery;
+    delete bQuery; // Deletes bQuery varible
+    return copy;
+  }
 }
